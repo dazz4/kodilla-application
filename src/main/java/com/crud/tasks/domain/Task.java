8bc9699675aa1cbd8dd68f03412c3df90a844fd6
dazz4 <kaminski.darek@icloud.com>
2019-06-23 11:06:32 +0100

    19.2

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String title;
+
+    @Column(name = "description")
+    private String content;
+}
